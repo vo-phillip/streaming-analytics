84dd891df7c500e0b16cce7e10da69b0ea8e7edd
Phillip Vo <phillip.vo@student.unimelb.edu.au>
2026-04-10 22:49:19 +1000

    feat: pushes Reddit posts to Redis while ignoring duplicates

--- ingestion/ingestor.py
+++ ingestion/ingestor.py
@@ -1,5 +1,8 @@
 import praw
-from config import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD, REDDIT_USER_AGENT
+from config import REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD, REDDIT_USER_AGENT, REDIS_HOST, REDIS_PORT
+import redis
+import json
+import time
 
 reddit = praw.Reddit(
     client_id=REDDIT_CLIENT_ID,
@@ -9,7 +12,16 @@ reddit = praw.Reddit(
     user_agent=REDDIT_USER_AGENT
 )
 
-posts = reddit.subreddit("leagueoflegends").new(limit=100)
-for post in posts:
-    print(f"Subreddit Title: {post.title}, Score: {post.score}")
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+
+while True:
+    posts = reddit.subreddit("leagueoflegends").new(limit=100)
+    for post in posts:
+        if r.sismember("seen_post_ids", post.id):
+            continue
+        print(f"Subreddit Title: {post.title}, Score: {post.score}")
+        post_data = json.dumps({"title": post.title, "score": post.score, "id": post.id, "created_utc": post.created_utc})
+        r.rpush("reddit_posts", post_data)
+        r.sadd("seen_post_ids", post.id)
+    time.sleep(60)
 
